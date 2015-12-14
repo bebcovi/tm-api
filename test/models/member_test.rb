84f6803249bfe240c790a23d25832e852c4aa866
Janko Marohnić <janko.marohnic@gmail.com>
2015-12-14 20:58:28 +0100

    Fix a test

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 describe Toastmasters::Models::Member do
+  include TestHelpers
+
   it "doesn't require an email" do
     Member.create(first_name: "Edward", last_name: "Daniels")
     Member.create(first_name: "Andrew", last_name: "Laeddis")
